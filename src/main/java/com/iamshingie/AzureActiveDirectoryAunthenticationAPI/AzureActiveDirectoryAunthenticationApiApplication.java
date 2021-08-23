df6d65a1dae9bbd4c353019504e0be7accfb82ec
iamshingie <shingiraideke@gmail.com>
2021-08-23 11:20:11 +0200

    Initial commit

---
+++ src/main/java/com/iamshingie/AzureActiveDirectoryAunthenticationAPI/AzureActiveDirectoryAunthenticationApiApplication.java
@@ -0,0 +1,13 @@
+package com.iamshingie.AzureActiveDirectoryAunthenticationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureActiveDirectoryAunthenticationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureActiveDirectoryAunthenticationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iamshingie/AzureActiveDirectoryAunthenticationAPI/AzureActiveDirectoryAunthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamshingie.AzureActiveDirectoryAunthenticationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureActiveDirectoryAunthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
